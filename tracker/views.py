021560d569002a226ac5a137bdc3205eb8af9915
Gerald Jepedro Sitorus <pedrositorur@gmail.com>
2025-12-31 20:45:59 +0700

    Desain tampilan dashboard dan fitur logout

--- tracker/views.py
+++ tracker/views.py
@@ -1,39 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.db.models import Sum  
+from django.db.models import Sum
 from .models import Transaction
 from .forms import TransactionForm
 
-@login_required
+@login_required 
 def transaction_list(request):
     if request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
             transaction = form.save(commit=False)
-            transaction.user = request.user
+            transaction.user = request.user 
             transaction.save()
             return redirect('transaction_list')
     else:
         form = TransactionForm()
 
-    # Ambil data transaksi
-    
     transactions = Transaction.objects.filter(user=request.user).order_by('-date')
 
-    # Total Pemasukan dan Pengeluaran
-    # aggregate(Sum('amount')) akan menghasilkan dictionary, misal: {'amount__sum': 50000}
-    # Saya pakai 'or 0' supaya kalau tidak ada data, nilainya jadi 0 (bukan None/Error)
-    
     total_income = transactions.filter(type='IN').aggregate(Sum('amount'))['amount__sum'] or 0
     total_expense = transactions.filter(type='OUT').aggregate(Sum('amount'))['amount__sum'] or 0
-
-    # Hitung Saldo
+    
     saldo = total_income - total_expense
 
-    return render(request, 'tracker/transaction_list.html', {
-        'form': form,
+    context = {
         'transactions': transactions,
-        'total_income': total_income,   # Kirim data ke HTML
-        'total_expense': total_expense, # Kirim data ke HTML
+        'form': form,
+        'total_income': total_income,
+        'total_expense': total_expense,
         'saldo': saldo
-    })
+    }
+    return render(request, 'tracker/transaction_list.html', context)
